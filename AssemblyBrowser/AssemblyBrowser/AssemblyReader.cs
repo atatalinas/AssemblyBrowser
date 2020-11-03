57b3095fdfbd2345801853fd02452b0774cc096b
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-11-04 00:13:17 +0300

    added AssemblyReader and RelayCommand

---
+++ AssemblyBrowser/AssemblyBrowser/AssemblyReader.cs
@@ -0,0 +1,49 @@
+﻿using AssemblyGetInfoLib;
+using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+
+namespace AssemblyBrowser
+{
+    public class AssemblyReader : INotifyPropertyChanged
+    {
+        public RelayCommand RelayCommand { get; private set; }
+        private ObservableCollection<Node> root;
+        private IGetInfo assemblyInfoGetter;
+
+        public AssemblyReader(ObservableCollection<Node> root, IGetInfo infoGetter)
+        {
+            this.root = root;
+            assemblyInfoGetter = infoGetter;
+            RelayCommand = new RelayCommand(OpenHandler);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private void OpenHandler(object param)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "(*.dll)|*.dll";
+            if (dialog.ShowDialog() == true)
+            {
+                root.Clear();
+                root.Add(assemblyInfoGetter.GetFileInfo(dialog.FileName));
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ AssemblyBrowser/AssemblyGetInfoLib/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyGetInfoLib
+{
+    public class Node
+    {
+        public Node(string content = "")
+        {
+            Content = content;
+            Nodes = new ObservableCollection<Node>();
+        }
+
+        public string Content { get; set; }
+        public ObservableCollection<Node> Nodes { get; set; }
+    }
+}
