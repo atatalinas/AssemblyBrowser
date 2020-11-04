357b85a1fc0434a1a977afee4ff8297657de42e4
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-11-04 11:54:20 +0300

    added unit tests

---
+++ AssemblyBrowser/AssemblyGetInfoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyGetInfoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyGetInfoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25a9c739-513e-4e59-b9ce-f1b89a4edec2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AssemblyBrowser/AssemblyGetInfoTest/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using AssemblyGetInfoLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssemblyGetInfoTest
+{
+    [TestClass]
+    public class Tests
+    {
+        private Node Setup()
+        {
+            IGetInfo getter = new AssemblyGetInfo();
+            return getter.GetInfoFromFile("TesterDll.dll");
+        }
+
+        [TestMethod]
+        public void LoadingAssemblyTest()
+        {
+            Node actual = Setup();
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void AssemblyNameTest()
+        {
+            Node actual = Setup();
+            Assert.IsTrue(actual.Content.StartsWith("TesterDll"));
+        }
+
+        [TestMethod]
+        public void TypeNameTest()
+        {
+            Node actual = Setup();
+            Assert.AreEqual("Tester", actual.Nodes[0].Nodes[0].Content);
+        }
+
+
+
+    }
+}
